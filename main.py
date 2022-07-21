11b44a7d493587bb98cac0996d36e7ba8a6f415f
mdshaifulislam <sislam@cocus.cloud>
2022-07-21 13:33:15 +0200

    Added the docker-compose.yml from basyxaas for the simple AAS server.

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ from schemathesis.checks import not_a_server_error, content_type_conformance, \
 
 # load openapi specification from local file.
 schema = schemathesis.from_path('open_api_specification_aas_v2.json')
+print(schema)
 schema.base_url = 'http://127.0.0.1:4001/aasServer/'
 
 
 @schema.parametrize()
-@settings(max_examples=4)
+@settings(max_examples=20)
 def test_api(case):
     case.call_and_validate(checks=(not_a_server_error, content_type_conformance, response_schema_conformance))
