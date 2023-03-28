1745d295365e2568b869417415beae84d83274c1
mdshaifulislam <sislam@cocus.cloud>
2023-03-28 09:46:15 +0200

    removed unnecessary files.

--- asset_administration_shells_test_suits/main.py
+++ asset_administration_shells_test_suits/main.py
@@ -1,14 +1,4 @@
 import logging
-
-from asset_administration_shells_test_suits.negative_tests.third.endpoints_preparation.negative_test_three import (
-    NegativeTestExecutorWithInvalidRequestBodyAndQueryParamValue,
-)
-from negative_tests.first_test.endpoints_preparation.negative_test_one import (
-    NegativeTestExecutor,
-)
-from negative_tests.second_test.endpoints_preparation.negative_test_two import (
-    NegativeTestExecutorWithInvalidRequestBody,
-)
 from positive_tests.endpoints_preparation.positive_test import PositiveTestExecutor
 from runner.runner import TestRunner
 from parsers.schema_parser import AasSchemaParser
@@ -18,18 +8,8 @@ if __name__ == "__main__":
         filename="log_files/central_log.log", filemode="w", level=logging.INFO
     )
     file_location = "aas_openapi_specifications/open_api_specification_aas_v2.json"
-    output_file_name = "test_reports/Test Report_fAAAST.txt"
-    output_file_name_negative_first = (
-        "test_reports/Test Report_fAAAST_negative_first.txt"
-    )
-    output_file_name_negative_second = (
-        "test_reports/Test Report_fAAAST_negative_second.txt"
-    )
-    output_file_name_negative_third = (
-        "test_reports/Test Report_fAAAST_negative_third.txt"
-    )
-    # base_url = "http://localhost:8080"
-    base_url = 'http://localhost:60011'
+    output_file_name = "test_reports/test_report_FAAAST_RESTA_API_server.txt"
+    base_url = "http://localhost:8080"
     aas = AasSchemaParser(file_location=file_location)
     TestRunner(
         aas_schema=aas,
@@ -38,40 +18,7 @@ if __name__ == "__main__":
         aas_path=f"{base_url}/shells/",
         concept_description_path=f"{base_url}/concept-descriptions/",
         sub_model_path=f"{base_url}/submodels/{{submodelIdentifier}}",
-        _id="OVGUAdmin",
-        password="liaadmin",
+        _id=None,
+        password=None,
         executor_class=PositiveTestExecutor,
-    ).start_test()
-    TestRunner(
-        aas_schema=aas,
-        output_file_name=output_file_name_negative_first,
-        base_url=base_url,
-        aas_path=f'{base_url}/shells/',
-        concept_description_path=f'{base_url}/concept-descriptions/',
-        sub_model_path=f'{base_url}/submodels/{{submodelIdentifier}}',
-        _id="OVGUAdmin",
-        password="liaadmin",
-        executor_class=NegativeTestExecutor
-    ).start_test()
-    TestRunner(
-        aas_schema=aas,
-        output_file_name=output_file_name_negative_second,
-        base_url=base_url,
-        aas_path=f'{base_url}/shells/',
-        concept_description_path=f'{base_url}/concept-descriptions/',
-        sub_model_path=f'{base_url}/submodels/{{submodelIdentifier}}',
-        _id=None,
-        password=None,
-        executor_class=NegativeTestExecutorWithInvalidRequestBody
-    ).start_test()
-    TestRunner(
-        aas_schema=aas,
-        output_file_name=output_file_name_negative_third,
-        base_url=base_url,
-        aas_path=f'{base_url}/shells/',
-        concept_description_path=f'{base_url}/concept-descriptions/',
-        sub_model_path=f'{base_url}/submodels/{{submodelIdentifier}}',
-        _id=None,
-        password=None,
-        executor_class=NegativeTestExecutorWithInvalidRequestBodyAndQueryParamValue
     ).start_test()
